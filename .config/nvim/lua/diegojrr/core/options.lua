7d546ec40cb8fe612b5ea649fb1a8b299c823318
Diego de Jesús Ramírez Rodríguez <diegojrr00@gmail.com>
2022-11-04 15:30:44 -0600

    current setup, nvim and zsh

---
+++ .config/nvim/lua/diegojrr/core/options.lua
@@ -0,0 +1,30 @@
+local opt = vim.opt
+
+-- Line number configuration
+opt.relativenumber = true
+opt.number = true
+
+-- tabs and indentation
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.autoindent = true
+
+-- line wrapping
+opt.wrap = false
+
+-- search settings
+opt.ignorecase = true
+opt.smartcase = true
+
+-- appearance
+opt.termguicolors = true
+-- opt.background = "dark"
+opt.signcolumn = "yes"
+
+-- backspace
+opt.backspace = "indent,eol,start"
+
+-- split windows
+opt.splitright = true
+opt.splitbelow = true
